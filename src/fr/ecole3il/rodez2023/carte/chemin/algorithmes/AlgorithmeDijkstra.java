45c4f0664762aecb75989159a32c62d862cd14cc
HugoAbadie3il <abadieh@3il.fr>
2024-03-28 18:51:56 +0100

    :memo: Javadoc for AlgoDijkstra

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeDijkstra.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeDijkstra.java
@@ -5,10 +5,22 @@ import fr.ecole3il.rodez2023.carte.elements.Noeud;
 
 import java.util.*;
 
+/**
+ * Algorithme de recherche de chemin de Dijkstra
+ * @param <E> Type générique de la valeur des noeuds
+ */
 public class AlgorithmeDijkstra<E> implements AlgorithmeChemin<E> {
 
+    /**
+     * Trouve le chemin le plus court entre deux noeuds d'un graphe
+     * @param graphe Graphe dans lequel chercher le chemin
+     * @param depart Noeud de départ
+     * @param arrivee Noeud d'arrivée
+     * @return Liste des noeuds composant le chemin le plus court
+     */
     @Override
     public List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee) {
+        // Initialisation des structures de données
         Map<Noeud<E>, Noeud<E>> predecesseurs = new HashMap<>();
         Map<Noeud<E>, Double> distances = new HashMap<>();
         PriorityQueue<Noeud<E>> file = new PriorityQueue<>(Comparator.comparing(distances::get));
@@ -20,6 +32,7 @@ public class AlgorithmeDijkstra<E> implements AlgorithmeChemin<E> {
         distances.put(depart, 0.0);
         file.add(depart);
 
+        // Exploration des noeuds
         while (!file.isEmpty()) {
             Noeud<E> current = file.poll();
 
@@ -34,6 +47,7 @@ public class AlgorithmeDijkstra<E> implements AlgorithmeChemin<E> {
             }
         }
 
+        // Reconstitution du chemin
         List<Noeud<E>> chemin = new ArrayList<>();
         Noeud<E> current = arrivee;
 
